2524bff5273afbf83910c735f7a0b98897fab575
Bina <benjaminbinyam500@gmail.com>
2025-03-05 20:47:49 +0300

    Add the dark mode feature using react icons

--- src/components/context.jsx
+++ src/components/context.jsx
@@ -5,15 +5,18 @@ const AppContext = createContext();
 export const AppProvider = ({ children }) => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-  const toggleTheme = () => {
+  const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", newDarkTheme);
+    console.log(body);
   };
   return (
     <AppContext.Provider
       value={{
         isDarkTheme,
-        toggleTheme,
+        toggleDarkTheme,
       }}
     >
       {children}
