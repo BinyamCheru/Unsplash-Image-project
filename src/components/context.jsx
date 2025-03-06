780e281d86220a7979d1d238eb02351a1bbaf41f
Bina <benjaminbinyam500@gmail.com>
2025-03-06 09:19:11 +0300

    Add dark mode functionality - using js - based on the users preference

--- src/components/context.jsx
+++ src/components/context.jsx
@@ -1,17 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  console.log(prefersDarkMode);
+  return prefersDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState("monkey");
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
-    const body = document.querySelector("body");
-    body.classList.toggle("dark-theme", newDarkTheme);
+    // const body = document.querySelector("body");
+    // body.classList.toggle("dark-theme", newDarkTheme);
   };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
+
   return (
     <AppContext.Provider
       value={{
@@ -21,7 +34,7 @@ export const AppProvider = ({ children }) => {
         setSearchTerm,
       }}
     >
-      {children} 
+      {children}
     </AppContext.Provider>
   );
 };
